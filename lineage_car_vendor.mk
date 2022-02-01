efc6039934b35eb9da16fec8bc6c8627f88866a0
Tom Pratt <tom.pratt@outlook.com>
2022-02-01 17:23:09 +0100

    Use v2 audiocontrol hal
    Change-Id: I8a9f69b2fc4e617b0006cc0e60e8aba29fa24fef

--- lineage_car_vendor.mk
+++ lineage_car_vendor.mk
@@ -1,6 +1,6 @@
 PRODUCT_PACKAGES += \
     android.hardware.automotive.vehicle@2.0-service \
-    android.hardware.automotive.audiocontrol@1.0-service \
+    android.hardware.automotive.audiocontrol@2.0-service \
 
 PRODUCT_COPY_FILES += \
     packages/services/Car/car_product/init/init.bootstat.rc:root/init.bootstat.rc \
