17d7e3e251b074b0d00c53319946f563bbceae7e
PortalRomOS <portalromOS@gmail.com>
2023-01-27 11:20:09 +0000

    change to portalrom

--- portalrom_car.mk
+++ portalrom_car.mk
@@ -1,5 +1,5 @@
 PRODUCT_PACKAGE_OVERLAYS += \
-    device/lineage/car/overlay \
+    device/portalrom/car/overlay \
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     config.override_forced_orient=true \
